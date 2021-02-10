ff2cde4af1c95b8525583446dc1710b43e6dfe0f
Edgar Giese <edgar@egiese.de>
2021-02-10 19:46:46 +0100

    Minimal .NET Core 5.0

--- ProgressConsoleApp/Program.cs
+++ ProgressConsoleApp/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using Progress.Open4GL.Exceptions;
 
 namespace ProgressConsoleApp
 {
-    class Program
+  class Program
+  {
+    private static Progress.Open4GL.Proxy.Connection pAConn;
+    private static pAMobileEtikett pAMEtk;
+    private static string connectionString = "AppServer://padb-01:5163/proalpha-test2-sfe";
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      string cTicket = null;
+      try
+      {
+        pAConn = new Progress.Open4GL.Proxy.Connection(connectionString, "", "", "");
+        pAConn.SessionModel = 1;
+        pAMEtk = new pAMobileEtikett(pAConn);
+
+        pAMEtk.paLogin("XMDE", "xGiese", "xmdepass", "", out cTicket);
+      }
+      catch (Open4GLException ex)
+      {
+        Console.WriteLine($"Open4GLException: {ex.Message}");
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Exception: {ex.Message} - {ex.InnerException} - {ex.StackTrace}");
+      }
+      finally
+      {
+        Console.WriteLine($"Hello {cTicket}!");
+        pAMEtk.pALogout(cTicket);
+        pAConn?.Dispose();
+        pAMEtk?.Dispose();
+      }
+
+
     }
+  }
 }
