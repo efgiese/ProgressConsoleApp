acba8ca7a07ab682b4240b66774b587b831680ed
Edgar Giese <edgar@egiese.de>
2021-02-10 18:24:26 +0100

    Init project

---
+++ ProgressConsoleApp/doc/pAMobileEtikett.cs
@@ -0,0 +1,27 @@
+#region Assembly pAMobileEtikett, Version=0.0.5.0, Culture=neutral, PublicKeyToken=null
+// pAMobileEtikett.dll
+#endregion
+
+/*
+
+using Progress.Open4GL.Proxy;
+using StrongTypesNS;
+
+public class pAMobileEtikett : AppObject
+{
+  public pAMobileEtikett();
+  public pAMobileEtikett(Connection connectObj);
+  public pAMobileEtikett(Connection connectObj, bool useWebConfigFile);
+  public pAMobileEtikett(string userId, string password, string appServerInfo);
+  public pAMobileEtikett(string urlString, string userId, string password, string appServerInfo);
+
+  # public string ActivityPosting(string gpcBCR_Connection_Obj, string pcFirma, int piRueckMeldeNr, int piArbeitsfolge, string pcMeldeart, int piMitarbeiter, decimal pdZeit, decimal pdGutMenge, decimal pdAusschussMenge, bool plAFO_Fertig, decimal pdMenge_TS, int piAnzahlRMS, bool plOnline, out bool plSuccess, out int piSrvDataLfdNr);
+  # public string GetABBelNrPos(string gpcBCR_Connection_Obj, string pcFirma, int piBelegNummer, decimal pdPosition, out tt_V_BelegPosTxtDataTable tt_V_BelegPosTxt, out bool plSuccess);
+  # public string GetActInformation(string gpcBCR_Connection_Obj, string pcFirma, int piRueckMeldeNr, int piArbeitsfolge, out tt_ActivityInfoDataTable tt_ActivityInfo, out bool plSuccess);
+  # public string GetTeileInfoLabel(string gpcBCR_Connection_Obj, string pcFirma, string pcArtikel, string pcSprache, out tt_TeileInfoLabelDataTable tt_TeileInfoLabel, out bool plSuccess);
+  # public string GetTextByRmNr(string gpcBCR_Connection_Obj, string pcFirma, int piRueckMeldeNr, out tt_V_BelegPosTxtDataTable tt_V_BelegPosTxt, out bool plSuccess);
+  # public string paLogin(string gpcServiceID, string gpcUserID, string gpcPassword, string gpcInfo, out string gopcSessionID);
+  # public string pALogout(string gpcBCR_Connection_Obj);
+}
+
+*/
---
+++ README.md
@@ -0,0 +1,11 @@
+# ProgressConsoleApp
+
+This project was generated on debian Linux 10 with .NET Core 5.0.103
+
+## doc
+
+Generated sourcefile from Progress ProxyGenerator
+
+## lib
+
+Generated library from Progress ProxyGenerator
